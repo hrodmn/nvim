c49b9d83329ae61aee01f71c47e6cd4edc838684
hrodmn <henry.rodman@gmail.com>
2024-11-24 06:17:06 -0600

    update config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -18,22 +18,6 @@ local servers = {
       },
     },
   },
-  -- pylsp = {
-  --   settings = {
-  --     pylsp = {
-  --       plugins = {
-  --         pyflakes = { enabled = false },
-  --         pycodestyle = { enabled = false },
-  --         autopep8 = { enabled = false },
-  --         yapf = { enabled = false },
-  --         mccabe = { enabled = false },
-  --         pylsp_mypy = { enabled = false },
-  --         pylsp_black = { enabled = false },
-  --         pylsp_isort = { enabled = false },
-  --       },
-  --     },
-  --   },
-  -- },
   ruff = {},
   terraformls = {},
   html = {},
