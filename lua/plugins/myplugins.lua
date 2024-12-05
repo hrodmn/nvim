692bae605dbf5bf5e9509f1096d65a2b9ff2d8eb
hrodmn <henry.rodman@gmail.com>
2024-12-05 05:26:13 -0600

    Add neogit

--- lua/plugins/myplugins.lua
+++ lua/plugins/myplugins.lua
@@ -251,5 +251,14 @@ local plugins = {
       },
     },
   },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+      "nvim-telescope/telescope.nvim", -- optional
+    },
+    config = true,
+  },
 }
 return plugins
